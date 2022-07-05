b2ad35cf6e03d28f4085edfd207210de6d22cda2
kusimura <kusimura@esm.co.jp>
2022-07-05 22:09:34 +0900

    chapter2-3確認

---
+++ chapter2/2-3/script(list2-13).js
@@ -0,0 +1,10 @@
+<script type="text/babel">
+let dom = document.querySelector('#root')
+let el = (
+    <div>
+        <h2>JSX sample</h2>
+        <p>This is sample message.</p>
+    </div>
+)
+ReactDOM.render(el, dom)
+</script>
